93406b5f8e9ce93957bf8b246c0e47d23d28e791
宋杰鹏 <1817811642@qq.com>
2021-12-03 23:06:04 +0800

    Test

---
+++ Test/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+    //找到小镇的法官
+
+    /*在一个小镇里，按从 1 到 n 为 n 个人进行编号。传言称，这些人中有一个是小镇上的秘密法官。
+    如果小镇的法官真的存在，那么：
+    小镇的法官不相信任何人。
+    每个人（除了小镇法官外）都信任小镇的法官。
+    只有一个人同时满足条件 1 和条件 2 。
+    给定数组 trust，该数组由信任对 trust[i] = [a, b] 组成，表示编号为 a 的人信任编号为 b 的人。
+    如果小镇存在秘密法官并且可以确定他的身份，请返回该法官的编号。否则，返回 -1。*/
+
+    class Solution {
+        public int findJudge(int n, int[][] trust) {
+            if (n == 1) return 1;
+            int[] arr = new int[n + 1];
+            for (int i = 0; i < trust.length; i++) {
+                arr[trust[i][1]]++;
+                arr[trust[i][0]]--;
+            }
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == n - 1) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
